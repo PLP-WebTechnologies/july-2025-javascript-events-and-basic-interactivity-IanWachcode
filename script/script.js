55163f9ac81294fb68031fee760229e01c9629fa
IanWachcode <ianwachira90@gmail.com>
2025-09-08 10:03:15 +0300

    assignment 6

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton');
+
+function ToggleTheme() {
+  // Toggle 'dark-theme' class on the <body> element
+  document.body.classList.toggle('dark-theme');
+}
